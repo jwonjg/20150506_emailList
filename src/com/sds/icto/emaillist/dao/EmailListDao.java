9131f8e2499333e8b23d71b536ad17c093351f80
jinwonjeong <jwonjg@naver.com>
2015-05-06 17:28:56 +0900

    no message

---
+++ src/com/sds/icto/emaillist/dao/EmailListDao.java
@@ -0,0 +1,15 @@
+package com.sds.icto.emaillist.dao;
+
+import java.util.List;
+
+import com.sds.icto.emaillist.vo.EmailListVo;
+
+public class EmailListDao {
+	public void insert(){
+		
+	}
+	
+	public List<EmailListVo> fetchList(){
+		return null;
+	}
+}
